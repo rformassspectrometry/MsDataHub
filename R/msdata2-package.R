f78efa9ee6af6c4b936df12fbe2a26da0b64caad
lgatto <laurent.gatto@gmail.com>
2022-11-08 23:29:25 +0100

    initial commit

---
+++ R/msdata2-package.R
@@ -0,0 +1,9 @@
+#' Title
+#' 
+#' @description
+#'
+#' @name msdata2-package
+#' @aliases msdata2-package msdata2
+#' @docType package
+#' @author
+NULL
---
+++ tests/testthat/test_metadata.R
@@ -0,0 +1,11 @@
+context("metadata validity")
+
+test_that("metadata is valid",
+{
+    if(!requireNamespace("ExperimentHubData", quietly = TRUE))
+        BiocManager::install("ExperimentHubData")
+
+    path <- find.package("")
+    metadata <- system.file("extdata", "metadata.csv", package = "msdata2")
+    expect_true(ExperimentHubData::makeExperimentHubMetadata(path, metadata))
+})
