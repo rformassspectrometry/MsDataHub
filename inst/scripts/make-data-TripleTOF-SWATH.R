9f39a27a9daf6c6885e0389781c43d26dc6b4cbf
lgatto <laurent.gatto@gmail.com>
2022-11-27 22:08:37 +0100

    sciex data script

--- inst/scripts/make-data-TripleTOF-SWATH.R
+++ inst/scripts/make-data-TripleTOF-SWATH.R
@@ -8,8 +8,12 @@ if (!require("msdata"))
 fls <- dir(system.file("TripleTOF-SWATH", package = "msdata"), full.name = TRUE)
 fls <- fls[basename(fls) %in% c("PestMix1_DDA.mzML", "PestMix1_SWATH.mzML")]
 
-## desitinations
-dest <- file.path("../../data/TripleTOF-SWATH", basename(fls))
+
+## destination
+destdir <- "../../data/TripleTOF-SWATH"
+if (!file.exists(destdir))
+    dir.create(destdir)
+dest <- file.path(destdir, basename(fls))
 
 ## copy and check
 status <- file.copy(from = fls, to = dest)
---
+++ inst/scripts/make-data-sciex.R
@@ -0,0 +1,21 @@
+## Files fro the sciex folder: 20171016_POOL_POS_1_105-134.mzML and
+## 20171016_POOL_POS_3_105-134.mzML
+
+if (!require("msdata"))
+    BiocManager::install("msdata")
+
+## files to be copied
+fls <- dir(system.file("sciex", package = "msdata"), full.name = TRUE)
+fls <- fls[basename(fls) %in% c("20171016_POOL_POS_1_105-134.mzML",
+                                "20171016_POOL_POS_3_105-134.mzML"))
+
+## destination
+destdir <- "../../data/sciex"
+if (!file.exists(destdir))
+    dir.create(destdir)
+dest <- file.path(destdir, basename(fls))
+
+## copy and check
+status <- file.copy(from = fls, to = dest)
+stopifnot(status)
+stopifnot(file.exists(dest))
