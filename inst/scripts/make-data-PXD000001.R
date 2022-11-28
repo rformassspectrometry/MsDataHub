5f507acc774c32f73b50412e2601a555dd5289d2
lgatto <laurent.gatto@gmail.com>
2022-11-28 21:31:13 +0100

    add PXD000001 data

---
+++ inst/scripts/make-data-PXD000001.R
@@ -0,0 +1,19 @@
+## PXD000001 files: raw mzML and identification data
+
+if (!require("msdata"))
+    BiocManager::install("msdata")
+
+fls <- c(
+    msdata::proteomics(full.names = TRUE, pattern = "TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01-20141210.mzML.gz"),
+    msdata::ident(full.names = TRUE))
+
+## destination
+destdir <- "../../data/PXD000001"
+if (!file.exists(destdir))
+    dir.create(destdir)
+dest <- file.path(destdir, basename(fls))
+
+## copy and check
+status <- file.copy(from = fls, to = dest)
+stopifnot(status)
+stopifnot(file.exists(dest))
