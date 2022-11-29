6e6e3e538d2ba9a4c6e98b0b28262bc0dc013fb5
Laurent Gatto <laurent.gatto@uclouvain.be>
2022-11-29 09:47:50 +0100

    start writing documentation

---
+++ R/PXD000001.R
@@ -0,0 +1,33 @@
+##' @title PXD000001 Proteomics Data
+##'
+##' @description
+##'
+##' The PXD000001 files are part of the first ProteomeXchange
+##' submission (Vizcaíno J.A. et al, 2014), and contain the following
+##' files.
+##'
+##' - TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01-20141210.mzML.gz:
+##'   an TMT6 6-plex LC-MSMS data containing 6 human spiked-in
+##'   proteins in a constant *Erwinia carotovora* protein backgroud. The
+##'   data is described in more details in Gatto and Christoforou
+##'   (2013).
+##'
+##' - TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01-20141210.mzid:
+##'   generated searching the raw data against the *Erwinia
+##'   carotovora* fasta database
+##'
+##' @seealso
+##'
+##' The [rpx](https://bioconductor.org/packages/3.16/rpx) package can
+##' be used to access and download any PRIDE/ProteomeXchange files.
+##'
+##' @references
+##'
+##' - Vizcaíno J.A. et al. *ProteomeXchange: globally co-ordinated
+##'   proteomics data submission and dissemination*, Nature
+##'   Biotechnology 2014, 32,
+##'   223–226. http://www.ncbi.nlm.nih.gov/pubmed/24727771
+##'
+##' - Gatto L. and Christoforou A. *Using R and Bioconductor for
+##'   proteomics data analysis*, Biochim Biophys Acta - Proteins and
+##'   Proteomics, 2013. http://www.ncbi.nlm.nih.gov/pubmed/23692960
---
+++ R/sciex.R
@@ -0,0 +1,32 @@
+##' @title AB Sciex LC-MS data files
+##'
+##' @description
+##'
+##' The *sciex* mzML files represent profile-mode LC-MS data of pooled
+##' human serum samples (the same pool being measured). The samples
+##' were analyzed by ultra high-performance liquid chromatography
+##' (UHPLC; Agilent 1290) coupled to a Q-TOF mass spectrometer
+##' (TripleTOF 5600+ AB Sciex). The chromatographic separation was
+##' based in hydrophilic interaction liquid chromatography (HILIC) and
+##' performed using an Waters Acquity BEH Amide, 100 x 2.1 mm column.
+##'
+##' The mass specctrometer was operated in full scan mode in the mass
+##' range from 50 to 1000 m/z and with an accumulation time of 250
+##' ms. The files represent a subset of spectra/scans from m/z 105 to
+##' 134 and from retention time 0 to 260 seconds. The files were
+##' generated in the same LC-MS run, but from different
+##' injections. Details on the individual files are provided below.
+##'
+##'
+##' Files:
+##'
+##' - 20171016_POOL_POS_1_105-134.mzML: profile-mode LC-MS data of
+##'   pooled human serum samples. Injection index: 1.
+##'
+##' - 20171016_POOL_POS_3_105-134.mzML: profile-mode LC-MS data of
+##'   pooled human serum samples. Injection index: 19.
+##'
+##'
+##' @author
+##'
+##' Sigurdur Smarason, Giuseppe Paglia and Johannes Rainer
