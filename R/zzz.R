39f2520da9187cdea1586bda8bccee9a7ea5a22f
lgatto <laurent.gatto@gmail.com>
2022-12-11 11:02:22 +0100

    udpate according to R4MS syntax style

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libmane, pkgname) {
-    fl <- system.file("extdata", "metadata.csv", package=pkgname)
-    titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+    fl <- system.file("extdata", "metadata.csv", package = pkgname)
+    titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
     ExperimentHub::createHubAccessors(pkgname, titles)
 }
