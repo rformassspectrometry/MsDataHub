6aabc6862e1c1c97a14e97aa78aaed62be334d6a
Laurent Gatto <laurent.gatto@uclouvain.be>
2022-12-14 08:08:49 +0100

    update MsDataHub man page

--- R/MsDataHub.R
+++ R/MsDataHub.R
@@ -2,9 +2,17 @@
 ##'
 ##' @description
 ##'
+##'
+##' The MsDataHub package provides example mass spectrometry data,
+##' peptide spectrum matches or quantitative data from proteomics and
+##' metabolomics experiments.
+##'
 ##' The [MsDataHub()] function returns a `data.frame` with all the
 ##' annotated datasets provided in the package. For details on these
 ##' individual datasets, refer to their respective manual pages.
+##'
+##' See the vignette and the respective manuals pages for more details
+##' about the package and the data themselves.
 ##'
 ##' @return A `data.frame` describing the data available in
 ##'     `MsDataHub`.
