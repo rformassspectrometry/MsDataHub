c3992c0496392c7b8351cadff12fce9f43465abd
Laurent Gatto <lgatto@protonmail.ch>
2026-04-15 14:39:56 +0200

    add man

---
+++ R/miscDiaDda.R
@@ -0,0 +1,56 @@
+##' @title Misc DDA and DIA datasets
+##'
+##' @name miscDdaDia
+##'
+##' @aliases Christoforou_2016_TMT_DDA_FragPipe_Fraction1_psm.tsv
+##' @aliases Christoforou_2016_TMT_DDA_FragPipe_Fraction2_psm.tsv
+##' @aliases Christoforou_2016_TMT_DDA_MaxQuant_evidence.txt
+##' @aliases Christoforou_2016_TMT_DDA_sage_results.sage.tsv
+##' @aliases Christoforou_2016_TMT_DDA_sage_tmt.tsv
+##' @aliases Derks_2022_plex_DIA_DIANN_report-subset.tsv
+##' @aliases Derks_2022_plex_DIA_DIANN_report_subset.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_FragPipe_A_1_psm.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_FragPipe_A_2_psm.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_FragPipe_A_3_psm.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_FragPipe_B_1_psm.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_FragPipe_B_2_psm.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_FragPipe_B_3_psm.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_MaxQuant_evidence.txt
+##' @aliases vanPuyvelde_2022_LFQ_DDA_MaxQuant_peptides.txt
+##' @aliases vanPuyvelde_2022_LFQ_DDA_MaxQuant_proteinGroups.txt
+##' @aliases vanPuyvelde_2022_LFQ_DDA_PEAKS_LFQ_report.csv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_sage_lfq.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DDA_sage_results.sage.tsv
+##' @aliases vanPuyvelde_2022_LFQ_DIA_DIANN_report.parquet
+##' @aliases vanPuyvelde_2022_LFQ_DIA_DIANN_report.tsv
+##'
+##' @description
+##'
+##' Various DDA and DIA experiments that were processed with various software to
+##' illustrate the `QFeatures::readQFeatures()` function.
+##'
+##' - `vanPuyvelde_2022_LFQ_*`: DDA and DIA data from van Puyvelde et
+##'    al. (2022). LFQ benchmarking experiment processed with different
+##'    software.
+##' - `Christoforou_2016_TMT_DDA_*`: data from Christoforou et
+##'   al. (2016). hyperLOPIT spatial proteomics experiment processed with
+##'   different software.
+##' - `Derks_2022_plex_DIA_DIANN_report_subset.tsv`: subset from Derks et
+##'    al. (2022) plexDIA data.
+##'
+##' Source files are available on Zenodo: https://zenodo.org/records/19137577
+##'
+##' @references
+##'
+##' - Van Puyvelde, B., Daled, S., Willems, S. et al. A comprehensive LFQ
+##'   benchmark dataset on modern day acquisition strategies in proteomics. Sci
+##'   Data 9, 126 (2022). https://doi.org/10.1038/s41597-022-01216-6.
+##' - Christoforou, A., Mulvey, C., Breckels, L. et al. A draft map of the mouse
+##'   pluripotent stem cell spatial proteome. Nat Commun 7, 9992
+##'   (2016). https://doi.org/10.1038/ncomms9992
+##' - Derks, J., Leduc, A., Wallmann, G. et al. Increasing the throughput of
+##'   sensitive proteomics by plexDIA. Nat Biotechnol 41, 50–59
+##'   (2023). https://doi.org/10.1038/s41587-022-01389-w
+##'
+##' @author Kristina Gomoryova and Laurent Gatto
+NULL
